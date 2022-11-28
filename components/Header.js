9ca9cc0ee0958b2e45bae6a8a1d6e47304ba4c1a
office <erffon@gmail.com>
2022-11-28 22:59:18 +0330

    some changes

--- components/Header.js
+++ components/Header.js
@@ -2,15 +2,11 @@ import Image from "next/image";
 import logo from "../public/assets/images/logo.png";
 const Header = () => {
   return (
-    <header className="relative h-10 my-4 container mx-10">
+    <header className="sticky my-4 container mx-10">
       {/* Left */}
-      <Image
-        src={logo}
-        fill
-        objectFit="contain"
-        objectPosition="left"
-        className=""
-      />
+      <div className="relative h-10 ">
+        <Image src={logo} fill objectFit="contain" objectPosition="left" />
+      </div>
       {/* Middle */}
       {/* Right */}
     </header>

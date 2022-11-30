9a34873de666ed01392f7204c2edffd52a55691a
office <erffon@gmail.com>
2022-11-30 20:23:43 +0330

    some changes in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
 import logo from "../public/assets/images/logo.png";
+import {BiSearch} from "react-icons/bi"
 const Header = () => {
   return (
-    <header className="sticky my-4 container mx-10 flex flex-row">
+    <header className="sticky py-6 container px-20 flex flex-row">
       {/* Left */}
       <div className="flex items-center ">
         <Image src={logo} priority alt="airbnb logo" width={35}/>
         <h1 className="text-airbnb font-bold text-2xl tracking-tight">airbnb</h1>
       </div>
       {/* Middle - search */}
+      <div className="flex items-center">
+        <button>Anywhere</button>
+        <button>Any week</button>
+        <button>Add guests</button>
+        <div>
+          <BiSearch/>
+        </div>
+      </div>
       {/* Right */}
     </header>
   );

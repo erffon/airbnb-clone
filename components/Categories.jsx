0efbdc0f7a9129fadd953f3f9b80f16e9a26443d
office <erffon@gmail.com>
2022-12-03 00:05:30 +0330

    some few changes to header

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -3,7 +3,7 @@ import {RiFireLine,RiBuilding3Line,RiAncientPavilionLine} from "react-icons/ri";
 
 const categoryList = [{icon:<RiFireLine className="text-2xl"/>,title:"Trending"},{icon:<RiBuilding3Line className="text-2xl"/>,title:"Tiny Homes"},{icon:<RiAncientPavilionLine className="text-2xl"/>,title:"Lakefront"}];
 
-const Categories = ({activeCategory =''}) => {
+const Categories = ({activeCategory}) => {
     const {query} =useRouter();
     const activeCat = (activeCategory == query || "Trending")?"text-text-primary font-semibold border-b-2 border-black":'';
     
@@ -11,7 +11,7 @@ const Categories = ({activeCategory =''}) => {
     return (
         <div className="flex container px-20 gap-10">
             {categoryList?.map(({icon,title})=>
-                <div className={`${activeCat} flex flex-col items-center gap-1 pb-2`}>
+                <div className={`${activeCategory==title?activeCat:'text-text-secondary'} flex flex-col items-center gap-1 pb-2`}>
                     {icon}
                     <h2 className="text-sm">{title}</h2>
                 </div>

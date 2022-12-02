7a206460d69cacf95d636fa29927b50ff5e61023
office <erffon@gmail.com>
2022-12-02 17:01:15 +0330

    categories component created

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,13 +1,26 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import {RiFireLine} from "react-icons/ri";
 
 
 
-const Categories = () => {
+const Categories = ({activeCategory}) => {
+    const {query} =useRouter();
+    const [activeCat, setActiveCat] = useState("");
+
+    switch (activeCategory) {
+        case query:
+            setActiveCat("text-text-primary")
+            break;
+        default:
+            break;
+    }
+
     return (
         <div className="flex">
             <div className="flex flex-col">
                 <RiFireLine/>
-                <h2>Trending</h2>
+                <h2 className={`${activeCat}`}>Trending</h2>
             </div>
         </div>
     );

4d4f13f03978374ceaecff2018256b9d610bac82
office <erffon@gmail.com>
2022-12-03 11:17:42 +0330

    some icons added -to OMG

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,11 @@
 import { useRouter } from "next/router";
-import {RiFireLine,RiBuilding3Line,RiAncientPavilionLine,RiLandscapeLine,RiRestaurantLine} from "react-icons/ri";
+import {RiFireLine,RiBuilding3Line,RiAncientPavilionLine,RiLandscapeLine,RiRestaurantLine,RiImage2Line,RiWalkLine} from "react-icons/ri";
+import {TbBeach} from "react-icons/tb";
+import {SiForestry} from "react-icons/si";
+import {MdOutlineDownhillSkiing,MdOutlineCabin} from "react-icons/md";
+import {GiUfo} from "react-icons/gi";
 
-const categoryList = [{icon:<RiFireLine className="text-2xl"/>,title:"Trending"},{icon:<RiBuilding3Line className="text-2xl"/>,title:"Tiny Homes"},{icon:<RiAncientPavilionLine className="text-2xl"/>,title:"Lakefront"},{icon:<RiLandscapeLine className="text-2xl"/>,title:"Camping"},{icon:<RiRestaurantLine className="text-2xl"/>,title:"Trending"}];
+const categoryList = [{icon:<RiFireLine className="text-2xl"/>,title:"Trending"},{icon:<RiBuilding3Line className="text-2xl"/>,title:"Tiny Homes"},{icon:<SiForestry className="text-2xl"/>,title:"Lakefront"},{icon:<RiLandscapeLine className="text-2xl"/>,title:"Camping"},{icon:<RiRestaurantLine className="text-2xl"/>,title:"Bed and breakfasts"},{icon:<RiImage2Line className="text-2xl"/>,title:"Amazing views"},{icon:<MdOutlineDownhillSkiing className="text-2xl"/>,title:"Ski in/out"},{icon:<TbBeach className="text-2xl"/>,title:"Beachfront"},{icon:<MdOutlineCabin className="text-2xl"/>,title:"Cabins"},{icon:<GiUfo className="text-2xl"/>,title:"OMG!"}];
 
 const Categories = ({activeCategory}) => {
     const {query} =useRouter();

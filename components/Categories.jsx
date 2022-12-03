e3ca0069e21954766c7993da55e59d2f5858a20f
office <erffon@gmail.com>
2022-12-03 08:09:01 +0330

    some icons added

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
-import {RiFireLine,RiBuilding3Line,RiAncientPavilionLine} from "react-icons/ri";
+import {RiFireLine,RiBuilding3Line,RiAncientPavilionLine,RiLandscapeLine,RiRestaurantLine} from "react-icons/ri";
 
-const categoryList = [{icon:<RiFireLine className="text-2xl"/>,title:"Trending"},{icon:<RiBuilding3Line className="text-2xl"/>,title:"Tiny Homes"},{icon:<RiAncientPavilionLine className="text-2xl"/>,title:"Lakefront"}];
+const categoryList = [{icon:<RiFireLine className="text-2xl"/>,title:"Trending"},{icon:<RiBuilding3Line className="text-2xl"/>,title:"Tiny Homes"},{icon:<RiAncientPavilionLine className="text-2xl"/>,title:"Lakefront"},{icon:<RiLandscapeLine className="text-2xl"/>,title:"Camping"},{icon:<RiRestaurantLine className="text-2xl"/>,title:"Trending"}];
 
 const Categories = ({activeCategory}) => {
     const {query} =useRouter();
@@ -9,11 +9,11 @@ const Categories = ({activeCategory}) => {
     
 
     return (
-        <div className="flex container px-20 gap-10">
+        <div className="flex container px-20 gap-9">
             {categoryList?.map(({icon,title})=>
                 <div className={`${activeCategory==title?activeCat:'text-text-secondary'} flex flex-col items-center gap-1 pb-2 hover:text-text-primary hover:border-b-2 transition ease-out duration-200`}>
                     {icon}
-                    <h2 className="text-sm">{title}</h2>
+                    <h2 className="text-xs font-semibold">{title}</h2>
                 </div>
             )}
             

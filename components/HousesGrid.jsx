1c43cba02b65774021b554470b1032ec4a9d3a45
office <erffon@gmail.com>
2022-12-16 00:59:57 +0330

    some issue with random number

--- components/HousesGrid.jsx
+++ components/HousesGrid.jsx
@@ -5,17 +5,18 @@ const HousesGrid = ({ houses }) => {
   return (
     <ul className="container flex flex-wrap justify-evenly">
       {houses.map(({ id, imageUrl, name, price, priceCurrency }) => {
+        const star = Math.floor(Math.random() * 5);
         return (
           <li key={id} className="w-80 relative rounded-xl overflow-hidden mt-8 flex flex-col justify-between text-sm">
             <figure className="w-full h-72">
-              <Image src={imageUrl} objectFit="cover" fill className="rounded-xl overflow-hidden !static" />
+              <Image src={imageUrl} fill className="rounded-xl overflow-hidden !static" />
             </figure>
             <div className="flex justify-between">
               <h3 className="font-semibold pt-2">{name.slice(0, 25)}</h3>
-              <p className="flex justify-center items-center">
+              <div className="flex justify-center items-center">
                 <AiFillStar className="w-10" />
-                {Math.floor(Math.random() * 5)}
-              </p>
+                {star}
+              </div>
             </div>
             <p className="text-text-secondary">45 km to De Alde Feanen National Park</p>
             <p className="text-text-secondary">Jan 21 - Jan 27</p>

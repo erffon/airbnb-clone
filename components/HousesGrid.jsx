2b715b7f4e83170e6cb5ac21c232d856f9dd445d
office <erffon@gmail.com>
2022-12-15 19:52:23 +0330

    stars added to title

--- components/HousesGrid.jsx
+++ components/HousesGrid.jsx
@@ -10,7 +10,13 @@ const HousesGrid = ({ houses }) => {
             <figure className="w-full h-72">
               <Image src={imageUrl} objectFit="cover" fill className="rounded-xl overflow-hidden !static" />
             </figure>
-            <h3 className="font-semibold pt-2">{name.slice(0, 25)}</h3>
+            <div className="flex justify-between">
+              <h3 className="font-semibold pt-2">{name.slice(0, 25)}</h3>
+              <p className="flex justify-center items-center">
+                <AiFillStar className="w-10" />
+                {Math.floor(Math.random() * 5)}
+              </p>
+            </div>
             <p className="text-text-secondary">45 km to De Alde Feanen National Park</p>
             <p className="text-text-secondary">Jan 21 - Jan 27</p>
             <div className="flex gap-3">

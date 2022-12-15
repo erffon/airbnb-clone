40ce09708e50f980efb5913b964ca5fa3f37d2c6
office <erffon@gmail.com>
2022-12-16 01:55:54 +0330

    star numbers bug fix

--- components/HousesGrid.jsx
+++ components/HousesGrid.jsx
@@ -4,7 +4,7 @@ import { AiFillStar } from 'react-icons/ai';
 const HousesGrid = ({ houses }) => {
   return (
     <ul className="container flex flex-wrap justify-evenly my-8">
-      {houses.map(({ id, imageUrl, name, price, priceCurrency }) => {
+      {houses.map(({ id, imageUrl, name, price, priceCurrency }, index) => {
         return (
           <li key={id} className="w-80 relative rounded-xl overflow-hidden mb-8 flex flex-col justify-between text-sm">
             <figure className="w-full h-72">
@@ -12,8 +12,9 @@ const HousesGrid = ({ houses }) => {
             </figure>
             <div className="flex justify-between">
               <h3 className="font-semibold pt-2">{name.slice(0, 25)}</h3>
-              <div className="flex justify-center items-center">
-                <AiFillStar className="w-10" />
+              <div className="flex items-center">
+                <AiFillStar className="w-10 -mx-2" />
+                4.5
               </div>
             </div>
             <p className="text-text-secondary">45 km to De Alde Feanen National Park</p>
